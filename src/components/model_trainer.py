bd300e26fa5f764fba68a68975244479dbdc00c0
Sourav <bhardwajsouav113@gmail.com>
2025-04-18 12:23:22 +0530

    ready model pusher

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -13,6 +13,8 @@ from src.entity.config_entity import ModelTrainerConfig
 from src.entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact, ClassificationMetricArtifact
 from src.entity.estimator import MyModel
 
+
+
 class ModelTrainer:
     def __init__(self, data_transformation_artifact: DataTransformationArtifact,
                  model_trainer_config: ModelTrainerConfig):
